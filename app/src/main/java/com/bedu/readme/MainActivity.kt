a153e3c77322193e03eed5cbb6072cd647c67731
genarokepkana <genaromarcosac@outlook.com>
2021-07-28 11:08:02 -0500

    Se cambia de activity main a act2_signin

--- app/src/main/java/com/bedu/readme/MainActivity.kt
+++ app/src/main/java/com/bedu/readme/MainActivity.kt
@@ -15,31 +15,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        createDBAdmins()
-        createDBBooks()
-        createDBArticle()
-        createDBMagazine()
-        var name = findViewById<TextView>(R.id.editTextName)
-        var userName = findViewById<TextView>(R.id.editTextUserName)
-        var email = findViewById<TextView>(R.id.editTextTextEmailAddress)
-        var password = findViewById<TextView>(R.id.editTextTextPassword)
-        var phone = findViewById<TextView>(R.id.editTextPhone)
-        var buttonRegistrar = findViewById<Button>(R.id.btnRegistrarse)
-
-        buttonRegistrar.setOnClickListener {
-            if( (name.text.toString()!="")  && (userName.text.toString()!="") &&   (email.text.toString()!="")   &&   (password.text.toString()!="")    &&
-                (phone.text.toString()!="")){
-                listUsr[countUsers]= User(countUsers+1,name.text.toString(),userName.text.toString(),password.text.toString(),email.text.toString())
-                println("El nuevo usuario es: nombre: ${listUsr[countUsers]?.getName()} usuario: ${listUsr[countUsers]?.getUserName()} " +
-                        "contraseña: ${listUsr[countUsers]?.getPassword() }  email: ${listUsr[countUsers]?.getEmail()}   ")
-                countUsers++
-            }else{
-                println("Debes de llenar todos los campos")
-
-            }
-        }
-
-
 
     }
 
