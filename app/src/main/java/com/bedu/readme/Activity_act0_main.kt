dd267fb4975a25687198e8f1090651af69ddc1a6
Dyeseniact <camposdyes@gmail.com>
2021-07-28 02:56:20 -0500

    Agrego splash a pantalla de inicio

--- app/src/main/java/com/bedu/readme/Activity_act0_main.kt
+++ app/src/main/java/com/bedu/readme/Activity_act0_main.kt
@@ -1,11 +1,28 @@
 package com.bedu.readme
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+@Suppress("DEPRECATION")
 class Activity_act0_main  : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_act0_main)
+        setContentView(R.layout.activity_act_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+
+        Handler().postDelayed({
+            startActivity(Intent(this, Act3_Home::class.java))
+            finish()
+        }, 1500)
     }
 }
---
+++ app/src/main/java/com/bedu/readme/Activity_act1_login.kt
@@ -0,0 +1,11 @@
+package com.bedu.readme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_act1_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_act1_login)
+    }
+}
