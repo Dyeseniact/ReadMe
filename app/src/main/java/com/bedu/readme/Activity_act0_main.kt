e63b8b48cb593d0c6e7e6e5e3d52909a79bbf1a8
Dyeseniact <camposdyes@gmail.com>
2021-07-28 02:07:07 -0500

    Pantalla de Login

---
+++ app/src/main/java/com/bedu/readme/Activity_act0_main.kt
@@ -0,0 +1,11 @@
+package com.bedu.readme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_act0_main  : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_act0_main)
+    }
+}
