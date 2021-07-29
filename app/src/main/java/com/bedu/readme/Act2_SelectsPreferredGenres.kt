23e26eecc76650cd3de1440602fbdfb7de7c658c
genarokepkana <genaromarcosac@outlook.com>
2021-07-29 17:49:39 -0500

    Se agrega secuencia para pasar de los generos al home

--- app/src/main/java/com/bedu/readme/Act2_SelectsPreferredGenres.kt
+++ app/src/main/java/com/bedu/readme/Act2_SelectsPreferredGenres.kt
@@ -1,7 +1,9 @@
 package com.bedu.readme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Act2_SelectsPreferredGenres : AppCompatActivity() {
 
@@ -10,6 +12,15 @@ class Act2_SelectsPreferredGenres : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_act2_selects_preferred_genres)
+        var button = findViewById<Button>(R.id.continuarGeneros)
+
+        button.setOnClickListener {
+            val intent = Intent(this,Act3_Home::class.java).apply{
+
+            }
+
+            startActivity(intent)
+        }
     }
 
 
