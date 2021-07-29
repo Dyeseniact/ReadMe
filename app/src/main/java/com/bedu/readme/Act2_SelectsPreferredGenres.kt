bd25cf25adf7119f609613081642ae49edb266fb
Janner <janner.work@gmail.com>
2021-07-28 23:49:36 -0500

    Agregando opciones de generos

---
+++ app/src/main/java/com/bedu/readme/Act2_SelectsPreferredGenres.kt
@@ -0,0 +1,17 @@
+package com.bedu.readme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Act2_SelectsPreferredGenres : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_act2_selects_preferred_genres)
+    }
+
+
+
+}
