5fdc210aa0c54cdcbb1b1c0d67a6a42f3d084508
Dyeseniact <camposdyes@gmail.com>
2021-08-12 02:36:59 -0500

    Agregando Terminos y Condiciones a Registro

--- app/src/main/java/com/bedu/readme/Act2_signin.kt
+++ app/src/main/java/com/bedu/readme/Act2_signin.kt
@@ -2,12 +2,12 @@ package com.bedu.readme
 
 import android.app.AlertDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
-import db.*
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import db.countUsers
+import db.listUsr
 import models.User
 
 
@@ -22,9 +22,12 @@ class Act2_signin : AppCompatActivity() {
         val password = findViewById<TextView>(R.id.editText_password_register)
         val phone = findViewById<TextView>(R.id.editText_phone_register)
         val buttonRegistrar = findViewById<Button>(R.id.btn_register_user)
+        val btnTerms = findViewById<TextView>(R.id.textView_terms)
+        var checkTerms = findViewById<CheckBox>(R.id.check_register)
+
 
         buttonRegistrar.setOnClickListener {
-            if( (name.text.trim().isNotEmpty()) && (userName.text.trim().isNotEmpty()) && (email.text.trim().isNotEmpty()) && (password.text.trim().isNotEmpty()) && (phone.text.trim().isNotEmpty())){
+            if((name.text.trim().isNotEmpty()) && (userName.text.trim().isNotEmpty()) && (email.text.trim().isNotEmpty()) && (password.text.trim().isNotEmpty()) && (phone.text.trim().isNotEmpty()) && checkTerms.isChecked){
                 listUsr[countUsers]= User(countUsers+1,name.text.toString(),userName.text.toString(),password.text.toString(),email.text.toString())
                 countUsers++
                 mensaje("¡Bienvenido a la comunidad!","Es lo que lees cuando no tienes que hacerlo, lo que determina lo que serás. -Oscar Wilde.")
@@ -32,9 +35,33 @@ class Act2_signin : AppCompatActivity() {
                 startActivity(intent)
                 finish()
             }else{
-                Toast.makeText(this,"Error, debes de llenar todos los campos", Toast.LENGTH_LONG).show()
+                Toast.makeText(this,"Error, debes de llenar todos los campos y aceptar los terminos y condiciones.", Toast.LENGTH_LONG).show()
             }
         }
+
+        btnTerms.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Terminos y Condiciones")
+            builder.setMessage("La actividad del usuario en la aplicación como publicaciones o comentarios estarán sujetos a los presentes términos y condiciones. El usuario se compromete a utilizar el contenido, productos y/o servicios de forma lícita, sin faltar a la moral o al orden público, absteniéndose de realizar cualquier acto que afecte los derechos de terceros o el funcionamiento del sitio web.\n" +
+                    "\n" +
+                    "El usuario se compromete a proporcionar información verídica en los formularios del sitio web.\n" +
+                    "\n" +
+                    "El acceso al sitio web no supone una relación entre el usuario y el titular del sitio web.\n" +
+                    "\n" +
+                    "El usuario manifiesta ser mayor de edad y contar con la capacidad jurídica de acatar los presentes términos y condiciones." +
+                    "Conforme a lo establecido en la Ley Federal de Protección de Datos Personales en Posesión de Particulares, el titular de compromete a tomar las medidas necesarias que garanticen la seguridad del usuario, evitando que se haga uso indebido de los datos personales que el usuario proporcione en el sitio web.\n" +
+                    "\n" +
+                    "El titular corroborará que los datos personales contenidos en las bases de datos sean correctos, verídicos y actuales, así como que se utilicen únicamente con el fin con el que fueron recabados.")
+            builder.setPositiveButton("Aceptar", null)
+
+            val dialog = builder.create()
+            dialog.show()
+        }
+
+
+
+
+
 
     }
     private fun mensaje(title:String,message:String){
@@ -49,5 +76,6 @@ class Act2_signin : AppCompatActivity() {
             .create()
             .show()
     }
+
 
 }
