96446055b3e2fd4b11f74b06987a88163965ea3c
Erick Vega <erick190796@hotmail.com>
2021-08-11 23:32:42 -0500

    La sesión queda abierta al inciiar sesión, agregué carátulas libros y modifciaciones en los RV de la ventana home

--- app/src/main/java/com/bedu/readme/Act1_login.kt
+++ app/src/main/java/com/bedu/readme/Act1_login.kt
@@ -1,7 +1,8 @@
 package com.bedu.readme
 
-import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -15,13 +16,12 @@ var runProgram = true
 var loginAnswer = ""
 var selection = 100
 
-class Activity_act1_login : AppCompatActivity() {
+class Act1_login : AppCompatActivity() {
+    private lateinit var sharedPreferences : SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        createDBAdmins()
-        createDBBooks()
-        createDBArticle()
-        createDBMagazine()
+
         setContentView(R.layout.activity_act1_login)
 
         var email = findViewById<EditText>(R.id.editText_email)
@@ -29,14 +29,31 @@ class Activity_act1_login : AppCompatActivity() {
         var btnLogin = findViewById<Button>(R.id.btn_login)
         var btnRegister = findViewById<Button>(R.id.btn_register)
 
+        sharedPreferences = getSharedPreferences("Preferencias", Context.MODE_PRIVATE)
+        editor = sharedPreferences.edit()
+
+        if(sharedPreferences.getBoolean("userLog",false)){
+            userLogin = User(
+                sharedPreferences.getInt("userId",5000),
+                sharedPreferences.getString("userName","")!!,
+                sharedPreferences.getString("userUserName","")!!,
+                sharedPreferences.getString("userPassword","")!!,
+                sharedPreferences.getString("userEmail","")!!,
+                sharedPreferences.getString("userTypeAccount","")!!,
+                arrayListOf(
+                    returnMyPreferences(sharedPreferences.getString("userPreferredBooks","")!!),
+                    returnMyPreferences(sharedPreferences.getString("userPreferredMagazine","")!!),
+                    returnMyPreferences(sharedPreferences.getString("userPreferredArticle","")!!))
+            )
+            continueHome()
+        }
+
         btnLogin.setOnClickListener {
 
             if ((email.text.trim().isNotEmpty()) && (password.text.trim().isNotEmpty())){
                 var loginAnswer = login(email.text.toString(),password.text.toString())
                 if(loginAnswer == "ok"){
-                    val intent = Intent(this,Act3_Home::class.java).apply{}
-                    startActivity(intent)
-                    finish()
+                    continueHome()
                 }else if (loginAnswer == "badUser"){
                     Toast.makeText(this,"Usuario no encontrado", Toast.LENGTH_LONG).show()
                     email.setText("")
@@ -55,6 +72,12 @@ class Activity_act1_login : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+
+    private fun continueHome(){
+        val intent = Intent(this,Act3_Home::class.java).apply{}
+        startActivity(intent)
+        finish()
     }
 
     private fun loginUser(email: String, password: String):String {
@@ -79,6 +102,22 @@ class Activity_act1_login : AppCompatActivity() {
             if(listUsr[i]?.getTypeAccount()== "user" && listUsr[i]?.getEmail() == email){
                 if( listUsr[i]?.getPassword() == password){
                     userLogin = listUsr[i]!!
+                    editor.putBoolean("userLog",true).apply()
+                    editor.putInt("userId", userLogin.getId()).apply()
+                    editor.putString("userName", userLogin.getName() ).apply()
+                    editor.putString("userUserName", userLogin.getUserName() ).apply()
+                    editor.putString("userPassword", userLogin.getPassword() ).apply()
+                    editor.putString("userEmail", userLogin.getEmail() ).apply()
+                    editor.putString("userTypeAccount", userLogin.getEmail() ).apply()
+                    editor.putString("userPreferredBooks",
+                        ", "+userLogin.preferredGenre[0].toString().replace("[","")
+                            .replace("]","")+",").apply()
+                    editor.putString("userPreferredMagazine",
+                        ", "+userLogin.preferredGenre[1].toString().replace("[","")
+                            .replace("]","")+",").apply()
+                    editor.putString("userPreferredArticle",
+                        ", "+userLogin.preferredGenre[2].toString().replace("[","")
+                            .replace("]","")+",").apply()
                     return "ok"
                 }else{
                     return "badPassword"
--- app/src/main/java/com/bedu/readme/adapters/RecyclerAdapter.kt
+++ app/src/main/java/com/bedu/readme/adapters/RecyclerAdapter.kt
@@ -4,24 +4,28 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bedu.readme.R
 import com.bedu.readme.diaglog
 import com.bedu.readme.models.LiteratureRV
+import com.squareup.picasso.Picasso
 
 class RecyclerAdapter(val context: Context, val literature: ArrayList<LiteratureRV>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
 
     class ViewHolder(view : View) : RecyclerView.ViewHolder(view){
         val nameLiterature = view.findViewById<TextView>(R.id.rv_title_lecture)
         val authorLiterature = view.findViewById<TextView>(R.id.rv_author_lecture)
+        val imageLiterature = view.findViewById<ImageView>(R.id.rv_image)
         val vistaClick = view.findViewById<CardView>(R.id.card_rv_book)
 
         // función bind que recibe un objeto Contact y a partir de el genera un contact_item colocando la información en le layout
-        fun bind(title: String, author:String){
-            nameLiterature.text = title
-            authorLiterature.text = author
+        fun bind(literarure: LiteratureRV){
+            nameLiterature.text = literarure.title
+            authorLiterature.text = literarure.author
+            if(literarure.image != "" ){ Picasso.get().load(literarure.image).into(this.imageLiterature)  }
         }
     }
 
@@ -32,7 +36,7 @@ class RecyclerAdapter(val context: Context, val literature: ArrayList<Literature
     }
 
     override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
-        holder.bind(literature[position]!!.title, literature[position]!!.author)
+        holder.bind(literature[position])
 
         holder.vistaClick.setOnClickListener { diaglog(context,LayoutInflater.from(context), literature[position]) }
 
